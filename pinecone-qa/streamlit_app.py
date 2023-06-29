e49ff47ac0996e2efa12d85dcd6f3f4f100e56b4
Denson <denson@users.noreply.github.com>
2023-06-29 14:42:57 -0600

    Update streamlit_app.py
    fixed the bug introduced by not having a document.

--- pinecone-qa/streamlit_app.py
+++ pinecone-qa/streamlit_app.py
@@ -27,7 +27,8 @@ if st.button("Submit"):
     # Validate inputs
     if not openai_api_key or not pinecone_api_key or not pinecone_env or not pinecone_index or not query:
         st.warning(f"Please upload the document and provide the missing fields.")
-    else:
+
+    elif: source_doc:
         try:
             # Save uploaded file temporarily to disk, load and split the file into pages, delete temp file
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
@@ -39,6 +40,18 @@ if st.button("Submit"):
             # Generate embeddings for the pages, insert into Pinecone vector database, and expose the index in a retriever interface
             pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
             embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+            vectordb = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index)
+            retriever = vectordb.as_retriever()
+
+            # Initialize the OpenAI module, load and run the Retrieval Q&A chain
+            llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+            qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=retriever)
+            response = qa.run(query)
+            
+            st.success(response)         
+    else:
+        try:
+            # initialize the vector data store
             vectordb = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index)
             retriever = vectordb.as_retriever()
 
