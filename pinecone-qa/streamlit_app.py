415d26bb6870c418abf6100ba7e37d0c7e0fb53e
Denson <denson@users.noreply.github.com>
2023-06-29 00:38:37 -0600

    Update streamlit_app.py
    tried a different way of importing environment variable.

--- pinecone-qa/streamlit_app.py
+++ pinecone-qa/streamlit_app.py
@@ -6,17 +6,18 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+moved where the environment variable is pulled
 
 # Streamlit app
 st.subheader('Generative Q&A with LangChain & Pinecone')
             
 # Get OpenAI API key, Pinecone API key and environment, and source document input
 with st.sidebar:
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
     openai_api_key = st.text_input("OpenAI API key", type="password")
-            
+    pinecone_api_key = st.text_input(PINECONE_API_KEY, type="password")        
     pinecone_api_key = st.text_input(PINECONE_API_KEY, type="password")
-    pinecone_env = st.text_input("Pinecone environment")
+    pinecone_env = st.text_input("{}".format(PINECONE_API_KEY))
     pinecone_index = st.text_input("Pinecone index name")
 source_doc = st.file_uploader("Upload source document", type="pdf", label_visibility="collapsed")
 query = st.text_input("Enter your query")
