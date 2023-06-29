d1432389cf6dbda838ac3b4916dd4d831830c1f8
denson <densonsmith2@gmail.com>
2023-06-29 15:21:38 -0600

    added code from PradipNichite

--- chat/qa_multi_text_or_pdf.py
+++ chat/qa_multi_text_or_pdf.py
@@ -9,22 +9,31 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
 def main():
-    st.set_page_config(page_title="QA with your PDF files", layout="wide")
-    st.title("🤖! Question Answering with your PDF files")
+
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+    PINECONE_INDEX = os.getenv("PINECONE_INDEX")
+
+
+
+    st.set_page_config(page_title="QA with a knowlege base", layout="wide")
+    st.title("🤖! Question Answering with a knowlege base")
 
     st.markdown("""
-    Step 1: Upload multiple PDF files \n
-    Step 2: Enter your OpenAI API key. This costs $$. You will need to set up billing info at [OpenAI](https://platform.openai.com/account). \n
-    Step 3: Type your question at the bottom and click "Run" \n
+    Enter your OpenAI API key. This costs $$. You will need to set up billing info at [OpenAI](https://platform.openai.com/account). \n
+    Type your question at the bottom and click "Run" \n
     """)
 
     file_inputs = st.file_uploader("Upload your PDF files", type=["pdf"], accept_multiple_files=True)
-    openai_key = st.text_input("Enter your OpenAI API Key")
+    openai_api_key = st.text_input("OpenAI API key", type="password")
+    pinecone_api_key = st.text_input("PINECONE_API_KEY", value="{}".format(PINECONE_API_KEY), type="password")        
+    pinecone_env = st.text_input("PINECONE_ENVIRONMENT", value="{}".format(PINECONE_ENVIRONMENT))
+    pinecone_index = st.text_input("PINECONE_INDEX", value="{}".format(PINECONE_INDEX))
     query = st.text_area("Enter your question")
     run_button = st.button("Run!")
 
     chain_type = st.radio("Chain type", ['stuff', 'map_reduce', "refine", "map_rerank"])
-    k = st.slider("Number of relevant chunks", 1, 5, 2)
+    k = st.slider("Number of relevant chunks", 1, 10, 2)
 
     if run_button and file_inputs and openai_key and query:
         os.environ["OPENAI_API_KEY"] = openai_key
---
+++ chat/utils.py
@@ -0,0 +1,40 @@
+from sentence_transformers import SentenceTransformer
+import pinecone
+import openai
+import streamlit as st
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+PINECONE_INDEX = os.getenv("PINECONE_INDEX")
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+index = pinecone.Index(PINECONE_INDEX)
+
+def find_match(input):
+    input_em = model.encode(input).tolist()
+    result = index.query(input_em, top_k=2, includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+def query_refiner(conversation, query):
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    openai_api_key=openai_api_key
+    )
+    return response['choices'][0]['text']
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
