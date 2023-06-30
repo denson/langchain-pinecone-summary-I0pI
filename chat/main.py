3465783e6ea5d284dc5abb195a4fe0c4c380c0f2
denson <densonsmith2@gmail.com>
2023-06-30 00:39:03 -0600

    fixed textcontainer

--- chat/main.py
+++ chat/main.py
@@ -113,27 +113,27 @@ def main():
             textcontainer = st.container()
 
 
-        with textcontainer:
-            query = st.text_input("Query: ", key="input")
-            if query:
-                with st.spinner("typing..."):
-                    conversation_string = get_conversation_string()
-                    # st.code(conversation_string)
-                    refined_query = query_refiner(conversation_string, query)
-                    st.subheader("Refined Query:")
-                    st.write(refined_query)
-                    context = find_match(refined_query)
-                    # print(context)  
-                    response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
-                st.session_state.requests.append(query)
-                st.session_state.responses.append(response) 
-        with response_container:
-            if st.session_state['responses']:
+            with textcontainer:
+                query = st.text_input("Query: ", key="input")
+                if query:
+                    with st.spinner("typing..."):
+                        conversation_string = get_conversation_string()
+                        # st.code(conversation_string)
+                        refined_query = query_refiner(conversation_string, query)
+                        st.subheader("Refined Query:")
+                        st.write(refined_query)
+                        context = find_match(refined_query)
+                        # print(context)  
+                        response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+                    st.session_state.requests.append(query)
+                    st.session_state.responses.append(response) 
+            with response_container:
+                if st.session_state['responses']:
 
-                for i in range(len(st.session_state['responses'])):
-                    message(st.session_state['responses'][i],key=str(i))
-                    if i < len(st.session_state['requests']):
-                        message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+                    for i in range(len(st.session_state['responses'])):
+                        message(st.session_state['responses'][i],key=str(i))
+                        if i < len(st.session_state['requests']):
+                            message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
 
 
