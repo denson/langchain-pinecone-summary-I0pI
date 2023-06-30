b7f6881b4adea2668ea45e9396337e47f6cd7c66
denson <densonsmith2@gmail.com>
2023-06-29 22:51:06 -0600

    added checks for creds

--- chat/main.py
+++ chat/main.py
@@ -58,6 +58,7 @@ def get_conversation_string():
 
 
 st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
+
 with st.sidebar:
 
     openai_api_key = st.text_input("OpenAI API key", type="password")
@@ -65,57 +66,71 @@ with st.sidebar:
     pinecone_env = st.text_input("PINECONE_ENVIRONMENT", value="{}".format(PINECONE_ENVIRONMENT))
     pinecone_index = st.text_input("PINECONE_INDEX", value="{}".format(PINECONE_INDEX))
 
-if 'responses' not in st.session_state:
-    st.session_state['responses'] = ["How can I assist you?"]
+if st.button("Enter credentials"):
+    # Validate inputs
+    if not openai_api_key or not pinecone_api_key or not pinecone_env or not pinecone_index:
+        st.warning(f"Please provide the missing fields.")
 
-if 'requests' not in st.session_state:
-    st.session_state['requests'] = []
+else:
 
-llm = ChatOpenAI(model_name="gpt-4-0613", openai_api_key=openai_api_key)
+    if 'responses' not in st.session_state:
+        st.session_state['responses'] = ["How can I assist you?"]
 
-if 'buffer_memory' not in st.session_state:
-            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+    if 'requests' not in st.session_state:
+        st.session_state['requests'] = []
 
+    llm = ChatOpenAI(model_name="gpt-4-0613", openai_api_key=openai_api_key)
 
-system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
-and if the answer is not contained within the text below, say 'I don't know'""")
-
-
-human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
-
-prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
-
-conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+    if 'buffer_memory' not in st.session_state:
+                st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
 
+    system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
+    and if the answer is not contained within the text below, say 'I don't know'""")
 
 
-# container for chat history
-response_container = st.container()
-# container for text box
-textcontainer = st.container()
+    human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+    prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+    conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
 
 
-with textcontainer:
-    query = st.text_input("Query: ", key="input")
-    if query:
-        with st.spinner("typing..."):
-            conversation_string = get_conversation_string()
-            # st.code(conversation_string)
-            refined_query = query_refiner(conversation_string, query)
-            st.subheader("Refined Query:")
-            st.write(refined_query)
-            context = find_match(refined_query)
-            # print(context)  
-            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
-        st.session_state.requests.append(query)
-        st.session_state.responses.append(response) 
-with response_container:
-    if st.session_state['responses']:
 
-        for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i],key=str(i))
-            if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
+    # container for chat history
+    response_container = st.container()
+    # container for text box
+    textcontainer = st.container()
+
+
+    with textcontainer:
+        query = st.text_input("Query: ", key="input")
+        if query:
+            with st.spinner("typing..."):
+                conversation_string = get_conversation_string()
+                # st.code(conversation_string)
+                refined_query = query_refiner(conversation_string, query)
+                st.subheader("Refined Query:")
+                st.write(refined_query)
+                context = find_match(refined_query)
+                # print(context)  
+                response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+            st.session_state.requests.append(query)
+            st.session_state.responses.append(response) 
+    with response_container:
+        if st.session_state['responses']:
+
+            for i in range(len(st.session_state['responses'])):
+                message(st.session_state['responses'][i],key=str(i))
+                if i < len(st.session_state['requests']):
+                    message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
+
+
+
+
+
+                    
 
           
