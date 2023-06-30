e3a3bbfd4b575dccc2f4310e90bf84c2f1ec8ee8
denson <densonsmith2@gmail.com>
2023-06-29 21:59:07 -0600

    Integrated the utils.py into main.py.

--- chat/main.py
+++ chat/main.py
@@ -9,7 +9,45 @@ from langchain.prompts import (
 )
 import streamlit as st
 from streamlit_chat import message
-from utils import *
+# from utils import *
+
+
+
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+index = pinecone.Index(PINECONE_INDEX)
+
+def find_match(input,top_k=5):
+    # input_em = model.encode(input).tolist()
+    EMBEDDING_MODEL = "text-embedding-ada-002"
+    input_em = openai.Embedding.create(
+                                            input=input,
+                                            model=EMBEDDING_MODEL,
+                                            )["data"][0]['embedding']
+    
+    result = index.query(input_em, top_k=top_k, includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+def query_refiner(conversation, query):
+
+    response = openai.Completion.create(
+    model="gpt-4-0613",
+    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    openai_api_key=openai_api_key
+    )
+    return response['choices'][0]['text']
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
 
 
 
@@ -29,7 +67,7 @@ if 'responses' not in st.session_state:
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="")
+llm = ChatOpenAI(model_name="gpt-4-0613", openai_api_key="")
 
 if 'buffer_memory' not in st.session_state:
             st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
